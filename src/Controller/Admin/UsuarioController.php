ecbb39511e475bc87715ba24b28699b1c17cd717
Jose Julio Matarín <jmatarin@wakari.es>
2022-06-07 20:09:49 +0200

    Security

--- src/Controller/Admin/UsuarioController.php
+++ src/Controller/Admin/UsuarioController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 /**
  * @Route("/admin/usuario")
@@ -21,8 +22,13 @@ class UsuarioController extends AbstractController
      */
     public function index(UsuarioRepository $usuarioRepository): Response
     {
+        if ($this->isGranted('ROLE_SUPERADMIN')) {
+            $usuarios = $usuarioRepository->findAll();
+        } else {
+            $usuarios = $usuarioRepository->findBy(['email' => $this->getUser()->getUserIdentifier()]);
+        }
         return $this->render('admin/usuario/index.html.twig', [
-            'usuarios' => $usuarioRepository->findAll(),
+            'usuarios' => $usuarios
         ]);
     }
 
@@ -74,15 +80,32 @@ class UsuarioController extends AbstractController
         UsuarioRepository $usuarioRepository,
         UserPasswordHasherInterface $passwordHasher
     ): Response {
-        $form = $this->createForm(UsuarioType::class, $usuario);
+        if (
+            !$this->isGranted('ROLE_SUPERADMIN')
+            && $usuario->getUserIdentifier() != $this->getUser()->getUserIdentifier()
+        ) {
+            throw $this->createAccessDeniedException('No puedes editar un usuario que no sea tuyo');
+        }
+        $oldPassword = $usuario->getPassword();
+        $oldRoles = $usuario->getRoles();
+        $form = $this->createForm(UsuarioType::class, $usuario, [
+            'isSuperadmin' => $this->isGranted('ROLE_SUPERADMIN')
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $hashedPassword = $passwordHasher->hashPassword(
-                $usuario,
-                $usuario->getPassword()
-            );
-            $usuario->setPassword($hashedPassword);
+            if ($usuario->getPassword() == '') {
+                $usuario->setPassword($oldPassword);
+            } else {
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $usuario,
+                    $usuario->getPassword()
+                );
+                $usuario->setPassword($hashedPassword);
+            }
+            if (!$this->isGranted('ROLE_SUPERADMIN')) {
+                $usuario->setRoles($oldRoles);
+            }
             $usuarioRepository->add($usuario, true);
 
             return $this->redirectToRoute('app_admin_usuario_index', [], Response::HTTP_SEE_OTHER);
--- src/Form/UsuarioType.php
+++ src/Form/UsuarioType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Usuario;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,16 +13,39 @@ class UsuarioType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        if ($options['isSuperadmin']) {
+            $choices = [
+                'Usuario normal' => 'ROLE_USERNORMAL',
+                'Administrador' => 'ROLE_ADMIN',
+                'Superadministrador' => 'ROLE_SUPERADMIN'
+            ];
+        } else {
+            $choices = [
+                'Usuario normal' => 'ROLE_USERNORMAL'
+            ];
+        }
+
         $builder
             ->add('email')
-            ->add('password')
+            ->add('password', PasswordType::class, [
+                'required' => false
+            ])
             ->add('nombre')
             ->add('perfil');
+
+        if ($options['isSuperadmin']) {
+            $builder->add('roles', ChoiceType::class, [
+                'choices' => $choices,
+                'multiple' => true,
+                'expanded' => true
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'isSuperadmin' => false,
             'data_class' => Usuario::class,
         ]);
     }
